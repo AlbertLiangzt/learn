d45ab74a172b023beaf9fa35830ad2061b5bd719
albertliangzt <albertliangzt@gmail.com>
2020-02-22 23:20:20 +0800

    import

--- python/12_classification_algorithm_LR/1_gradient_test.py
+++ python/12_classification_algorithm_LR/1_gradient_test.py
@@ -9,3 +9,8 @@ import operator
 from numpy import *
 from common_libs import *
 import matplotlib.pyplot as plt
+
+# 输入数据
+Input = file2matrix("testSet.txt","\t")
+target = Input[:,-1] #获取分类标签列表
+[m,n] = shape(Input)
