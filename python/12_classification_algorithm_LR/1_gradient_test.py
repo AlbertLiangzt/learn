60b5b30306222ca404cbd1d2560847aeccce40c1
albertliangzt <albertliangzt@gmail.com>
2020-02-22 21:18:41 +0800

    import pak to gradient_test

---
+++ python/12_classification_algorithm_LR/1_gradient_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import math
+import numpy as np
+import operator
+from numpy import *
+from common_libs import *
+import matplotlib.pyplot as plt
