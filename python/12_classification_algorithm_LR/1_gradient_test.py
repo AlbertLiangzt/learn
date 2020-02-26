8fa89d0dfcd7ca9d23998bbe581cedfd99eeb25c
albertliangzt <albertliangzt@gmail.com>
2020-02-26 23:44:02 +0800

    add notes

--- python/12_classification_algorithm_LR/1_gradient_test.py
+++ python/12_classification_algorithm_LR/1_gradient_test.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 import os
 import sys
 import math
@@ -11,33 +9,33 @@ from common_libs import *
 import matplotlib.pyplot as plt
 
 # 输入数据
-Input = file2matrix("testSet.txt","\t")
-target = Input[:,-1] #获取分类标签列表
-[m,n] = shape(Input)
+Input = file2matrix("testSet1.txt", "\t")
+target = Input[:, -1]  # 获取分类标签列表
+[m, n] = shape(Input)  # m-行数 n-列数
+
 
 # 按分类绘制散点图
-drawScatterbyLabel(plt,Input)
+drawScatterbyLabel(plt, Input)
 
 # 构建x+b 系数矩阵：b这里默认为1
-dataMat = buildMat(Input)
-alpha = 0.001 # 步长
+dataMat = buildMat(Input)  # 矩阵的第一列都为1
+alpha = 0.001  # 步长
 steps = 500  # 迭代次数
 
-weights = ones((n,1))# 初始化权重向量
+weights = ones((n, 1))  # 初始化权重向量
 
 # 主程序
 for k in xrange(steps):
     # wx
-    gradient = dataMat*mat(weights)
+    gradient = dataMat * mat(weights)
     # sigmoid(wx)
     output = logistic(gradient)  # logistic function
-    errors = target-output # 计算误差
-    weights = weights + alpha*dataMat.T*errors
+    errors = target - output  # 计算误差
+    weights = weights + alpha * dataMat.T * errors
 
-print weights # 输出权重
+print weights  # 输出权重
 
-X = np.linspace(-5,5,100)
-#y=w*x+b: b:weights[0]/weights[2]; w:weights[1]/weights[2]
-Y = -(double(weights[0])+X*(double(weights[1])))/double(weights[2])
-plt.plot(X,Y)
+X = np.linspace(-5, 5, 100)
+Y = -(double(weights[0]) + X * (double(weights[1]))) / double(weights[2])
+plt.plot(X, Y)
 plt.show()
