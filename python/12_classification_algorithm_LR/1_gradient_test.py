f265a756aabd15a022f1d5e95f3c1c065df66f55
albertliangzt <albertliangzt@gmail.com>
2020-02-27 23:23:23 +0800

    add notes

--- python/12_classification_algorithm_LR/1_gradient_test.py
+++ python/12_classification_algorithm_LR/1_gradient_test.py
@@ -9,7 +9,7 @@ from common_libs import *
 import matplotlib.pyplot as plt
 
 # 输入数据
-Input = file2matrix("testSet1.txt", "\t")
+Input = file2matrix("testSet.txt", "\t")
 target = Input[:, -1]  # 获取分类标签列表
 [m, n] = shape(Input)  # m-行数 n-列数
 
@@ -18,20 +18,19 @@ target = Input[:, -1]  # 获取分类标签列表
 drawScatterbyLabel(plt, Input)
 
 # 构建x+b 系数矩阵：b这里默认为1
-dataMat = buildMat(Input)  # 矩阵的第一列都为1
+dataMat = buildMat(Input)  # 矩阵的第一列都为1,其他为原始数据
 alpha = 0.001  # 步长
 steps = 500  # 迭代次数
 
-weights = ones((n, 1))  # 初始化权重向量
+weights = ones((n, 1))  # 初始化权重向量 n行1列，每个元素都为1
+
 
 # 主程序
 for k in xrange(steps):
-    # wx
-    gradient = dataMat * mat(weights)
-    # sigmoid(wx)
-    output = logistic(gradient)  # logistic function
-    errors = target - output  # 计算误差
-    weights = weights + alpha * dataMat.T * errors
+    gradient = dataMat * mat(weights) # wx
+    prediction = logistic(gradient)  # logistic function --sigmoid(wx)
+    errors = target - prediction  # 计算误差
+    weights = weights + alpha * dataMat.T * errors  # 更新权重 [nL,1L] + [nL, mL]*[mL, 1L]
 
 print weights  # 输出权重
 
