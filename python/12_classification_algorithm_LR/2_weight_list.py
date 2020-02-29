798cf1d3967a6a7dba9f21b735619b82c2c126ca
albertliangzt <albertliangzt@gmail.com>
2020-02-29 16:54:14 +0800

    weight_list

--- python/12_classification_algorithm_LR/2_weight_list.py
+++ python/12_classification_algorithm_LR/2_weight_list.py
@@ -1,4 +1,6 @@
+#!/usr/local/bin/python
 # -*- coding: utf-8 -*-
+
 import os
 import sys
 import numpy as np
@@ -12,17 +14,20 @@ Input = file2matrix("testSet.txt","\t")
 target = Input[:,-1] #获取分类标签列表
 [m,n] = shape(Input)
 
+
 # 2.按分类绘制散点图
 drawScatterbyLabel(plt,Input)
 
+
 # 3.构建b+x 系数矩阵：b这里默认为1
 dataMat = buildMat(Input)
+
 
 # 4. 定义步长和迭代次数
 alpha = 0.001 # 步长
 steps = 500  # 迭代次数
 weights = ones((n,1))# 初始化权重向量
-weightlist = []
+weight_list = []
 
 # 5. 主程序
 for k in xrange(steps):
@@ -30,18 +35,20 @@ for k in xrange(steps):
     output = logistic(gradient)  # logistic函数
     errors = target-output # 计算误差
     weights = weights + alpha*dataMat.T*errors
-    weightlist.append(weights)
+    weight_list.append(weights)
 
 print weights # 输出训练后的权重
+
 # 6. 绘制训练后超平面
-X = np.linspace(-5,5,100)
+X = np.linspace(-5, 5, 100)
 Ylist=[]
-lenw = len(weightlist)
+lenw = len(weight_list)
 for indx in xrange(lenw):
     if indx%100 == 0:   # 每20次输出一次分类超平面
-        weight = weightlist[indx]
+        weight = weight_list[indx]
         Y=-(double(weight[0])+X*(double(weight[1])))/double(weight[2])
         plt.plot(X,Y)
         #分类超平面注释
         plt.annotate("hplane:"+str(indx),xy = (X[99],Y[99]))
+		
 plt.show()
