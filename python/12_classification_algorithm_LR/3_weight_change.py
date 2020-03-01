646565bd33d52059591b9871ed124ff6475933b3
albertliangzt <albertliangzt@gmail.com>
2020-03-01 22:01:25 +0800

    format

--- python/12_classification_algorithm_LR/3_weight_change.py
+++ python/12_classification_algorithm_LR/3_weight_change.py
@@ -16,30 +16,34 @@ target = Input[:, -1]  # 获取分类标签列表
 
 # 3.构建b+x 系数矩阵：b这里默认为1
 dataMat = buildMat(Input)
+
 # print dataMat
 # 4. 定义步长和迭代次数
 alpha = 0.001  # 步长
 steps = 500  # 迭代次数
 weights = ones((n, 1))  # 初始化权重向量
-weightlist = []
+weight_list = []
+
 # 5. 主程序
 for k in xrange(steps):
     gradient = dataMat * mat(weights)  # 梯度
     output = logistic(gradient)  # logistic函数
     errors = target - output  # 计算误差
     weights = weights + alpha * dataMat.T * errors
-    weightlist.append(weights)
+    weight_list.append(weights)
 
-print weights  # 输出训练后的权重
+print weights
 fig = plt.figure()
 axes1 = plt.subplot(311)
 axes2 = plt.subplot(312)
 axes3 = plt.subplot(313)
 weightmat = mat(zeros((steps, n)))
 i = 0
-for weight in weightlist:
+
+for weight in weight_list:
     weightmat[i, :] = weight.T
     i += 1
+
 X = linspace(0, steps, steps)
 # 输出前10个点的截距变化
 axes1.plot(X, weightmat[:, 0], color='blue', linewidth=1, linestyle="-")
--- python/12_classification_algorithm_LR/4_predict.py
+++ python/12_classification_algorithm_LR/4_predict.py
@@ -12,11 +12,11 @@ weights = mat([[4.17881308], [0.50489874], [0.61980264]])
 # testdata = mat([-0.147324,2.874846])
 # testdata = mat([-2.46015, -6.866805])
 testdata = mat([0.931635, -1.589505])
-# 0.931635  -1.589505   1.0
 
 m, n = shape(testdata)
 testmat = zeros((m, n + 1))
 testmat[:, 0] = 1
 testmat[:, 1:] = testdata
-print classifier(testmat, weights)
-# print regression_calc(testmat,weights)
+
+print "classification: ", classifier(testmat, weights)
+print "regression calculate: ", regression_calc(testmat, weights)
