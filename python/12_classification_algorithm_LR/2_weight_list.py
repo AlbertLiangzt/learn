b2942a59367df7e465bd142afe23e5fcee1746d6
albertliangzt <albertliangzt@gmail.com>
2020-03-04 23:23:42 +0800

    add explanation

--- python/12_classification_algorithm_LR/2_weight_list.py
+++ python/12_classification_algorithm_LR/2_weight_list.py
@@ -1,54 +1,41 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
-
 import os
 import sys
+import math
 import numpy as np
 import operator
 from numpy import *
 from common_libs import *
 import matplotlib.pyplot as plt
 
-# 1.导入数据
-Input = file2matrix("testSet.txt","\t")
-target = Input[:,-1] #获取分类标签列表
-[m,n] = shape(Input)
+# 输入数据
+Input = file2matrix("testSet.txt", "\t")
+target = Input[:, -1]  # 获取分类标签列表
+[m, n] = shape(Input)  # m-行数 n-列数
 
 
-# 2.按分类绘制散点图
-drawScatterbyLabel(plt,Input)
+# 按分类绘制散点图
+drawScatterbyLabel(plt, Input)
 
-
-# 3.构建b+x 系数矩阵：b这里默认为1
-dataMat = buildMat(Input)
-
-
-# 4. 定义步长和迭代次数
-alpha = 0.001 # 步长
+# 构建x+b 系数矩阵：b这里默认为1
+dataMat = buildMat(Input)  # 矩阵的第一列都为1,其他为原始数据
+alpha = 0.001  # 步长
 steps = 500  # 迭代次数
-weights = ones((n,1))# 初始化权重向量
-weight_list = []
 
-# 5. 主程序
+weights = ones((n, 1))  # 初始化权重向量 n行1列，每个元素都为1
+
+
+# 主程序
 for k in xrange(steps):
-    gradient = dataMat*mat(weights) # 梯度
-    output = logistic(gradient)  # logistic函数
-    errors = target-output # 计算误差
-    weights = weights + alpha*dataMat.T*errors
-    weight_list.append(weights)
+    gradient = dataMat * mat(weights) # wx
+    prediction = logistic(gradient)  # logistic function --sigmoid(wx)
+    errors = target - prediction  # 计算误差
+    weights = weights + alpha * dataMat.T * errors  # 更新权重 [nL,1L] + [nL, mL]*[mL, 1L]
 
-print weights # 输出训练后的权重
+print weights  # 输出权重
 
-# 6. 绘制训练后超平面
 X = np.linspace(-5, 5, 100)
-Ylist=[]
-lenw = len(weight_list)
-for indx in xrange(lenw):
-    if indx%100 == 0:   # 每20次输出一次分类超平面
-        weight = weight_list[indx]
-        Y=-(double(weight[0])+X*(double(weight[1])))/double(weight[2])
-        plt.plot(X,Y)
-        #分类超平面注释
-        plt.annotate("hplane:"+str(indx),xy = (X[99],Y[99]))
-		
+Y = -(double(weights[0]) + X * (double(weights[1]))) / double(weights[2])
+plt.plot(X, Y)
 plt.show()
