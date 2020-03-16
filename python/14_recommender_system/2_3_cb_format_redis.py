2e62b837aa3c948ca91c8d5082008fea2414431f
albertliangzt <albertliangzt@gmail.com>
2020-03-17 01:32:08 +0800

    format cb_result

---
+++ python/14_recommender_system/2_3_cb_format_redis.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python
+# --coding:utf-8--
+
+input_file = "./data/2_2_cb_result.data"
+output_file = "./data/2_3_cb_redis.data"
+
+out_file = open(output_file, "w")
+
+item_dic = {}
+PRE_STR = "CB_"
+
+# item-item_list的形式
+with open(input_file, "r") as fd:
+    for line in fd:
+        item_id_a, item_id_b, score = line.strip().split("\t")
+
+        if item_id_a not in item_dic:
+            item_dic[item_id_a] = []
+        item_dic[item_id_a].append((item_id_b, score))
+
+# SET CB_itemA itemB:scoreAB_itemC:scoreAC_itemD:scoreAD
+for k, v in item_dic.items():
+    key = PRE_STR + k
+
+    list = sorted(v, key=lambda x: x[1], reverse=True)[:100] # 取第二列降序
+    val = '_'.join([':'.join([str(temp[0]), str(round(float(temp[1]), 6))]) for temp in list])
+
+    out_file.write(" ".join(['SET', key, val]))
+    out_file.write("\n")
+out_file.close()
--- python/14_recommender_system/readme.txt
+++ python/14_recommender_system/readme.txt
@@ -18,11 +18,15 @@ data.rar需要先解压
 			desc1_1	itemid_1	score3
 			desc1_2	itemid_1	score4
 			tag1	itemid_1	score5
-	2.2协同过滤（详看09_recommendation_algorithm），得到item-item的相似度
+	2.2协同过滤（详看09_recommendation_algorithm），得到item-item的相似度(好几种方式跑下来的数据都是5.3g应该没问题吧)
 		IN ->	token itemid score
 		OUT->	itemid_1 itemid_2 simlar_score
+	2.3数据格式化
+		IN->	itemid_1 itemid_2 simlar_score
+		OUT->	item_1 -> item_2:score1_item3:score2
 3、【召回】CF算法
 4、【排序】Sklearn
 
 python 1_merge_meta.py
 pyrhon 2_1_cb_trains.py
+python 2_3_cb_format_redis.py
