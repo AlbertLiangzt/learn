6470e16e40e1aa554e2de2241f33070b7fbdc934
albertliangzt <albertliangzt@gmail.com>
2020-04-20 15:12:12 +0800

    run_spark.sh

---
+++ spark/01_test/run_spark.sh
@@ -0,0 +1,14 @@
+
+/usr/local/src/hadoop-2.6.5/bin/hadoop fs -rmr /userWatchList_out
+
+/usr/local/src/spark-1.6.0-bin-hadoop2.6/bin/spark-submit \
+	--master spark://master:7077 \
+	--num-executors 2 \
+	--executor-memory 1g \
+	--executor-cores 1 \
+	--driver-memory 1g \
+	--class UserWatchList /usr/local/src/learn/albert/15_scala_test/com.albert.scala.test-1.0-SNAPSHOT.jar
+
+
+
+
