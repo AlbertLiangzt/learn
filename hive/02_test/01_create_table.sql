f14474f647a7994d4b4fb6a0548d381dc064359a
albertliangzt <albertliangzt@gmail.com>
2020-04-26 11:07:50 +0800

    create table unsual

---
+++ hive/02_test/01_create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE rating_table(
+userid string,
+movieid string,
+rating string,
+ts string
+)
+row format delimited fields terminated by ','
+stored as textfile
+location '/rating_table'
