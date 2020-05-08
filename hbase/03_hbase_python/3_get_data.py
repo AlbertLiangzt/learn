50d88d467bf04a6474c63789b1238ca2136641cd
albertliangzt <albertliangzt@gmail.com>
2020-05-08 21:43:48 +0800

    get data

---
+++ hbase/03_hbase_python/3_get_data.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+from hbase import Hbase
+from hbase.ttypes import *
+
+transport = TSocket.TSocket('master', 9090)
+transport = TTransport.TBufferedTransport(transport)
+
+protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+client = Hbase.Client(protocol)
+
+transport.open()
+
+#--------------------
+
+tableName = 'new_music_table'
+rowKey = '1001'
+
+result = client.getRow(tableName, rowKey, None)
+
+for r in result:
+	print 'the row is: ', r.row
+	print 'the name is: ', r.columns.get('meta_data:name').value
+	print 'the flag is: ', r.columns.get('flags:is_valid').value
+
+
+
