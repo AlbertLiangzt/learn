f4c8fd1ddbf8cd6e41d8b5bf08dc8a886fc5d397
albertliangzt <albertliangzt@gmail.com>
2020-05-24 16:30:17 +0800

    shell

---
+++ storm/01_test/run_01_test.sh
@@ -0,0 +1,12 @@
+python /usr/local/src/apache-storm-0.9.3/bin/storm jar \
+	/usr/local/src/learn/albert/23_storm/01_test.jar \
+	WordCount \
+	local
+	#remote
+# 注意，被注释的需放在后面
+# 	local
+# 	#remote
+
+# 	remote
+#	#local
+
