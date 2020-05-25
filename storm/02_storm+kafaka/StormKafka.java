c1e0355cb9038ff35d4f5d47e99c0fea6f280a0c
albertliangzt <albertliangzt@gmail.com>
2020-05-25 20:35:23 +0800

    StormKafka.java

---
+++ storm/02_storm+kafaka/StormKafka.java
@@ -0,0 +1,44 @@
+package stormKafka;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.topology.TopologyBuilder;
+import storm.kafka.*;
+
+public class StormKafka {
+    public static void main(String[] args) throws Exception {
+        String topic = "storm_kafka";
+        String zkRoot = "/storm_kafka";
+        String spoutId = "kafkaSport";
+
+        BrokerHosts brokerHosts = new ZkHosts("master:2181");
+        SpoutConfig kafkaConf = new SpoutConfig(brokerHosts, topic, zkRoot, spoutId);
+        kafkaConf.forceFromStart = true;
+        kafkaConf.scheme = new SchemeAsMultiScheme(new StringScheme());
+
+        KafkaSpout kafkaSpout = new KafkaSpout(kafkaConf);
+
+        TopologyBuilder topoBuilder = new TopologyBuilder();
+
+        topoBuilder.setSpout("spout", kafkaSpout, 2);
+        topoBuilder.setBolt("printer", new PrinterBolt())
+                .shuffleGrouping("spout");
+
+        Config conf = new Config();
+        conf.setDebug(false);
+
+        if (args != null && args.length > 0) {
+            conf.setNumWorkers(3);
+            StormSubmitter.submitTopology(args[0], conf, topoBuilder.createTopology());
+        } else {
+            conf.setMaxTaskParallelism(3);
+
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("kafka", conf, topoBuilder.createTopology());
+        }
+
+    }
+
+}
