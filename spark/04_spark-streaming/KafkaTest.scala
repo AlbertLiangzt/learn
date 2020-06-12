d2c1b1c3e060c48f84014d029c04d357fbf077a2
albertliangzt <albertliangzt@gmail.com>
2020-06-12 22:17:37 +0800

    KafkaTest.scala

---
+++ spark/04_spark-streaming/KafkaTest.scala
@@ -0,0 +1,9 @@
+package com.albert.streaming.test
+
+import org.apache.spark.SparkConf
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object KafkaTest {
+  
+}
