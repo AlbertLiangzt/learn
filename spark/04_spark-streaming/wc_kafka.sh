1585735e82483523bf38e4eb10b8f232a5606133
albertliangzt <albertliangzt@gmail.com>
2020-06-12 22:18:45 +0800

    KafkaTest

---
+++ spark/04_spark-streaming/wc_kafka.sh
@@ -0,0 +1,6 @@
+/usr/local/src/spark-1.6.0-bin-hadoop2.6/bin/spark-submit \
+	--master local[2] \
+	--class com.albert.streaming.test.KafkaTest /usr/local/src/learn/albert/25_spark_streaming/streaming-1.0-SNAPSHOT.jar \
+	master \
+	9999
+
