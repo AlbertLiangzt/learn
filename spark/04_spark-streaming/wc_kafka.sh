19dd8a114d82046072d0d4bc46455d217b197c03
albertliangzt <albertliangzt@gmail.com>
2020-06-14 23:43:12 +0800

    moiffy spark-streaming-kafka version

--- spark/04_spark-streaming/wc_kafka.sh
+++ spark/04_spark-streaming/wc_kafka.sh
@@ -1,6 +1,6 @@
 /usr/local/src/spark-1.6.0-bin-hadoop2.6/bin/spark-submit \
 	--master local[2] \
 	--class com.albert.streaming.kafka.KafkaTest /usr/local/src/learn/albert/25_spark_streaming/streaming-1.0-SNAPSHOT.jar \
-	--jars /usr/local/src/spark-1.6.0-bin-hadoop2.6/lib/spark-streaming-kafka_2.10-1.6.0.jar \
+	--jars /usr/local/src/spark-1.6.0-bin-hadoop2.6/lib/spark-streaming-kafka_2.11-1.6.0.jar \
 	master \
 	9999
