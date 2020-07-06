2633d96071cfb2da61216a651a93bb0268aa5aea
albertliangzt <albertliangzt@gmail.com>
2020-07-06 11:07:35 +0800

    add notes

--- python/12_classification_algorithm_LR/1_gradient_calculate.py
+++ python/12_classification_algorithm_LR/1_gradient_calculate.py
@@ -21,7 +21,7 @@ drawScatterbyLabel(plt, Input)
 # 构建x+b 系数矩阵：b这里默认为1
 dataMat = buildMat(Input)  # 矩阵的第一列都为1,其他为原始数据
 alpha = 0.001  # 步长
-steps = 500  # 迭代次数
+steps = 500  # 迭代次数(避免无休止运行)
 
 weights = ones((n, 1))  # 初始化权重向量 n行1列，每个元素都为1
 
