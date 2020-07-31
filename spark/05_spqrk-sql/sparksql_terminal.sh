0303bd01c13c57e322b6efa99e995b3e14101923
albertliangzt <albertliangzt@gmail.com>
2020-07-31 10:12:56 +0800

    enter terminal of spark_sql

---
+++ spark/05_spqrk-sql/sparksql_terminal.sh
@@ -0,0 +1,8 @@
+/usr/local/src/spark-1.6.0-bin-hadoop2.6/bin/spark-sql \
+	--master local[2] \
+	--jars /usr/local/src/spark-1.6.0-bin-hadoop2.6/lib/mysql-connector-java-5.1.41-bin.jar
+
+/usr/local/src/spark-1.6.0-bin-hadoop2.6/bin/spark-submit \
+	--master local[2] \
+	--jars /usr/local/src/spark-1.6.0-bin-hadoop2.6/lib/mysql-connector-java-5.1.41-bin.jar \
+
